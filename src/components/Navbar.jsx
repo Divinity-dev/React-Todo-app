a9d327a171523ec77e14573568a1bf4dccbefe04
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-03-02 11:59:00 +0100

    Add todo components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+// import "./styles.css";
+import { useState, useEffect, useRef } from "react";
+import { useOnClickOutside } from '../useOnclickOutside';
+
+const Navbar = () => {
+  const [dropdown, setDropdown] = useState(false);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (dropdown && ref.current && !ref.current.contains(event.target)) {
+        setDropdown(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+    useOnClickOutside(ref, dropdown, () => setDropdown(false));
+
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", handler);
+    };
+  }, [dropdown]);
+
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li ref={ref}>
+          <button onClick={() => setDropdown((prev) => !prev)}>
+            Services <span>&#8595;</span>
+          </button>
+          {dropdown && (
+            <ul>
+              <li>Design</li>
+              <li>Development</li>
+            </ul>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
